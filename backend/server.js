77ba41b6542dad1677344a49fde3d2dbe4e705db
jbienzeisler@ukaachen.de <jonas@UK-2024920055.>
2025-05-09 09:51:24 +0200

    UI Fixes, Fixed docker-compose

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
 
 // Serve built frontend
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 // Config endpoint
 app.get('/api/config', (req, res) => {
